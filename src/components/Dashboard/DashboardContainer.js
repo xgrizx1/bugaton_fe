dbb5281ef6ea26e85c34bcff8271ee5702be03a8
aleksandar.ogrizovic@symphony.is <aleksandar.ogrizovic@symphony.is>
2017-11-04 15:17:04 +0100

    Installed victorian

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -2,49 +2,43 @@ import React from 'react';
 import CustomPanel from 'components/Common/Panel';
 import PropTypes from 'prop-types';
 import './dashboard.scss';
+import OverallFeel from 'components/Dashboard/OverallFeel';
 
-const Section = ({sectionName}) =>
+const Section = ({ sectionName }) => (
   <div className="dashboard-section">
     <div className="dashboard-section-name"> {sectionName}</div>
-    <div className="dashboard-section-line"/>
-  </div>;
+    <div className="dashboard-section-line" />
+  </div>
+);
 
 Section.propTypes = {
-  sectionName: PropTypes.string.isRequired
+  sectionName: PropTypes.string.isRequired,
 };
 
 class DashboardContainer extends React.Component {
   render() {
     return (
       <div className="dashboard-container">
-        <Section sectionName={'Overall'}/>
+        <Section sectionName={'Overall'} />
         <div className="row">
           <div className="col-md-6">
-            <CustomPanel
-              title={'Flags'}
-            >
+            <CustomPanel title={'Flags'}>
               <div>Description</div>
             </CustomPanel>
           </div>
           <div className="col-md-6">
-            <CustomPanel
-              title={'Feel'}
-            >
-              <div>Description</div>
-            </CustomPanel>
+            <OverallFeel />
           </div>
         </div>
-        <Section sectionName={'Ducks'}/>
+        <Section sectionName={'Ducks'} />
         <div className="row">
           <div className="col-xs-12">
-            <CustomPanel
-              title={'Live Feed'}
-            >
+            <CustomPanel title={'Live Feed'}>
               <div>Description</div>
             </CustomPanel>
           </div>
         </div>
-        <Section sectionName={'Profile'}/>
+        <Section sectionName={'Profile'} />
       </div>
     );
   }
---
+++ src/components/Dashboard/OverallFeel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomPanel from '../Common/Panel';
+
+const OverallFeel = () => {
+  return (
+    <CustomPanel title={'Feel'}>
+      <div>Description</div>
+    </CustomPanel>
+  );
+};
+
+export default OverallFeel;

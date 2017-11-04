edd4ee589c70a24745f00fffbc8685bd8b0c6089
aleksandar.ogrizovic@symphony.is <aleksandar.ogrizovic@symphony.is>
2017-11-04 09:46:07 +0100

    First commit

---
+++ src/client.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
+import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
+import App from './containers/App';
+import configureStore from './stores';
+
+
+const store = configureStore();
+
+ReactDOM.render(
+  <AppContainer>
+    <Provider store={store}>
+      <Router
+        history={browserHistory}
+        onUpdate={() => {
+          window.scrollTo(0, 0);
+        }}
+      >
+        <Route name="app" path="/" component={App}/>
+      </Router>
+    </Provider>
+  </AppContainer>,
+  document.getElementById('app')
+);
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const NextApp = require('./containers/App').default; // eslint-disable-line global-require
+
+    ReactDOM.render(
+      <AppContainer>
+        <Provider store={store}>
+          <NextApp/>
+        </Provider>
+      </AppContainer>,
+      document.getElementById('app')
+    );
+  });
+}
